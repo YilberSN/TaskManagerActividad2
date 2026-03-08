ebf69fbda930d469e2d8ad79bca0f6922575c0b4
Yilber <yilber.serrano@gmail.com>
2026-03-08 17:15:58 -0400

    Middleware was implemented and routes were updated

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TaskController;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Rutas Laravel UI
 Auth::routes();
+
+Route::get('/home', function(){
+    return redirect()->route('tasks.index');
+});
+
+Route::middleware('auth')->group(function(){
+    Route::resource('tasks', TaskController::class);
+});
 
 Route::resource('tasks', App\Http\Controllers\TaskController::class);
 
-Route::get('/', function(){
-    return redirect()->route('tasks.index');
-});
+
+// CRUD DE TAREAS EXITOSO
+//Route::get('/', function(){
+//    return redirect()->route('tasks.index');
+//});
 
 
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
